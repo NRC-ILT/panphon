e0bddce6e003c380079b6444bef6aa4693206894
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2026-03-12 15:22:34 -0400

    build: set version to 0.20.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ilt-panphon',
-      version='0.20.1',
+      version='0.20.2',
       description='Tools for using the International Phonetic Alphabet with phonological features',
       url='https://github.com/nrc-ilt/panphon',
       long_description=open('README.md', encoding='utf-8').read(),
